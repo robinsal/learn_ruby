3412dc7c8db966c6a0178d0baaa473e8f49279df
Alex Robinson <robinsal@umd.edu>
2019-10-07 11:02:30 -0400

    first and second done

---
+++ 01_temperature/temperature.rb
@@ -0,0 +1,7 @@
+def ftoc(deg)
+    (deg - 32) * 5.0 / 9
+end
+
+def ctof(deg)
+    deg * 9.0 / 5 + 32
+end
---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,15 @@
+def add(n1, n2)
+    n1 + n2
+end
+
+def subtract(n1, n2)
+    n1 - n2
+end
+
+def sum(arr)
+    total = 0
+    for i in arr do
+    	total += i
+    end
+    total
+end
